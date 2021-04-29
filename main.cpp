cd1c9e6390d4e00a9aa2447b0fc8293a228a13ea
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2021-04-29 16:55:16 +0200

    déplacement de mario ok

--- main.cpp
+++ main.cpp
@@ -11,38 +11,98 @@
 
 int main(int argc, char **argv)
 {
-		sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	//varioble-------------------------------------------------------------------------------------------------------------
+	int vitesseMario = 2; //récler le vitesse de déplacement de Mario
+	//----------------------------------------------------------------------------------------------------------------
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	window.setFramerateLimit(60);
+	sf::Texture Mario;
+	sf::Sprite sprite_Mario;
 
-		// on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+	if (!Mario.loadFromFile("image/mario_bas.gif")) {
+		printf("imposible de charger mario bas");
+	}
+	sprite_Mario.setTexture(Mario);
 
-		//----------------------------phase test ---------------------------
-		sf::Texture texture;
-		if (!texture.loadFromFile("image/menu.jpg"))
-			return EXIT_FAILURE;
-		sf::Texture texture2;
-		if (!texture2.loadFromFile("image/mur.jpg"))
-			return EXIT_FAILURE;
-		sf::Sprite sprite(texture);
-		//-------------------------------------------------------------------------
 
-		while (window.isOpen())
-		{
-			// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
-			sf::Event event;
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
+			window.setSize(sf::Vector2u(50, 60));
+		}
 
-			while (window.pollEvent(event))
-			{
 
-				// évènement "fermeture demandée" : on ferme la fenêtre
-				if (event.type == sf::Event::Closed)
-					window.close();
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+			if (!Mario.loadFromFile("image/mario_haut.gif")) {
+				printf("imposible de charger mario haut");
+			}
+			sprite_Mario.move(0, -vitesseMario);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+			if (!Mario.loadFromFile("image/mario_bas.gif")) {
+				printf("imposible de charger mario bas");
+			}
+			sprite_Mario.move(0, vitesseMario);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+			if (!Mario.loadFromFile("image/mario_gauche.gif")) {
+				printf("imposible de charger mario gauche");
 			}
 
-			//----------------------------------------------
-			window.clear();
-			window.draw(sprite);
-			window.display();
-			//---------------------------------------------------
+			sprite_Mario.move(-vitesseMario, 0);
 		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+			if (!Mario.loadFromFile("image/mario_droite.gif")) {
+				printf("imposible de charger mario droite");
+			}
+
+			sprite_Mario.move(vitesseMario, 0);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+			if (!Mario.loadFromFile("image/mario_haut.gif")) {
+				printf("imposible de charger mario haut");
+			}
+			sprite_Mario.move(0, -vitesseMario);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+			if (!Mario.loadFromFile("image/mario_bas.gif")) {
+				printf("imposible de charger mario bas");
+			}
+			sprite_Mario.move(0, vitesseMario);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+			if (!Mario.loadFromFile("image/mario_gauche.gif")) {
+				printf("imposible de charger mario gauche");
+			}
+			sprite_Mario.move(-vitesseMario, 0);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+			if (!Mario.loadFromFile("image/mario_droite.gif")) {
+				printf("imposible de charger mario droite");
+			}
+			sprite_Mario.move(vitesseMario, 0);
+		}
+
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+			window.close();
+		}
+
+
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+		// Draw the sprite
+		window.draw(sprite_Mario);
+		// Update the window
+		window.display();
+	}
 		return 0;
 }
