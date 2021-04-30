4ebbfde9b8c5a4d3917105fda19af147b50ab2ac
kylian-duval <70692979+kylian-duval@users.noreply.github.com>
2021-04-30 11:39:07 +0200

    jeu séparer du menu

--- jeu.h
+++ jeu.h
@@ -1,1 +1,54 @@
-#pragma once
+
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+	if (!Mario.loadFromFile("image/mario_haut.gif")) {
+		printf("imposible de charger mario haut");
+	}
+	sprite_Mario.move(0, -vitesseMario);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+	if (!Mario.loadFromFile("image/mario_bas.gif")) {
+		printf("imposible de charger mario bas");
+	}
+	sprite_Mario.move(0, vitesseMario);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+	if (!Mario.loadFromFile("image/mario_gauche.gif")) {
+		printf("imposible de charger mario gauche");
+	}
+
+	sprite_Mario.move(-vitesseMario, 0);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+	if (!Mario.loadFromFile("image/mario_droite.gif")) {
+		printf("imposible de charger mario droite");
+	}
+
+	sprite_Mario.move(vitesseMario, 0);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+	if (!Mario.loadFromFile("image/mario_haut.gif")) {
+		printf("imposible de charger mario haut");
+	}
+	sprite_Mario.move(0, -vitesseMario);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+	if (!Mario.loadFromFile("image/mario_bas.gif")) {
+		printf("imposible de charger mario bas");
+	}
+	sprite_Mario.move(0, vitesseMario);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+	if (!Mario.loadFromFile("image/mario_gauche.gif")) {
+		printf("imposible de charger mario gauche");
+	}
+	sprite_Mario.move(-vitesseMario, 0);
+}
+else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+	if (!Mario.loadFromFile("image/mario_droite.gif")) {
+		printf("imposible de charger mario droite");
+	}
+	sprite_Mario.move(vitesseMario, 0);
+}
+if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+	menu = 0;
+}
--- main.cpp
+++ main.cpp
@@ -27,59 +27,7 @@ int main()
 		sf::Event event;
 		while (menu == 1)
 		{
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-				if (!Mario.loadFromFile("image/mario_haut.gif")) {
-					printf("imposible de charger mario haut");
-				}
-				sprite_Mario.move(0, -vitesseMario);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-				if (!Mario.loadFromFile("image/mario_bas.gif")) {
-					printf("imposible de charger mario bas");
-				}
-				sprite_Mario.move(0, vitesseMario);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-				if (!Mario.loadFromFile("image/mario_gauche.gif")) {
-					printf("imposible de charger mario gauche");
-				}
-
-				sprite_Mario.move(-vitesseMario, 0);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-				if (!Mario.loadFromFile("image/mario_droite.gif")) {
-					printf("imposible de charger mario droite");
-				}
-
-				sprite_Mario.move(vitesseMario, 0);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
-				if (!Mario.loadFromFile("image/mario_haut.gif")) {
-					printf("imposible de charger mario haut");
-				}
-				sprite_Mario.move(0, -vitesseMario);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-				if (!Mario.loadFromFile("image/mario_bas.gif")) {
-					printf("imposible de charger mario bas");
-				}
-				sprite_Mario.move(0, vitesseMario);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-				if (!Mario.loadFromFile("image/mario_gauche.gif")) {
-					printf("imposible de charger mario gauche");
-				}
-				sprite_Mario.move(-vitesseMario, 0);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-				if (!Mario.loadFromFile("image/mario_droite.gif")) {
-					printf("imposible de charger mario droite");
-				}
-				sprite_Mario.move(vitesseMario, 0);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-				menu = 0;
-			}
+			#include "jeu.h";
 			// Clear screen
 			window.clear();
 			// Draw the sprite
@@ -124,7 +72,11 @@ int main()
 
 		}
 
-		
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 	}
 
 }
